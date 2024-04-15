484ce3a2608c4441c4809a88996dfb317033578f
HH45137 <3387640174@qq.com>
2024-04-15 21:35:06 +0800

    modified:   scripts/build-android-arm64.sh 	modified:   scripts/build-android-armv7a.sh

--- scripts/build-android-arm64.sh
+++ scripts/build-android-arm64.sh
@@ -10,5 +10,5 @@ tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz --skip-old-files
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
 
-python3 ./waf configure -T release --prefix=../android/build --android=aarch64,host,21 --target=../android/aarch64 --disable-warns --togles
+python3 ./waf configure -T release --prefix=/android_build --android=aarch64,host,21 --target=/android_build/aarch64 --disable-warns --togles
 python3 ./waf install --strip
--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -10,7 +10,7 @@ tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz --skip-old-files
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
 
-python3 ./waf configure -T release --prefix=../android/build --android=armeabi-v7a-hard,host,21 --target=../android/armeabi-v7a --disable-warns -4 --togles
+python3 ./waf configure -T release --prefix=/android_build --android=armeabi-v7a-hard,host,21 --target=/android_build/armeabi-v7a --disable-warns -4 --togles
 python3 ./waf install --strip
 
 # # 移除多余lib
