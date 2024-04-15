11f762c104d129ad67b93769f02f780d65941c4a
HH45137 <3387640174@qq.com>
2024-04-15 22:12:12 +0800

    try install lib

--- scripts/build-android-arm64.sh
+++ scripts/build-android-arm64.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 git submodule init && git submodule update
 
+sudo apt-get update
+sudo apt-get install -f -y libopenal-dev g++-multilib gcc-multilib libpng-dev libjpeg-dev libfreetype6-dev libfontconfig1-dev libcurl4-gnutls-dev libsdl2-dev zlib1g-dev libbz2-dev libedit-dev
+
 wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -nc    
 wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -nc
 
@@ -8,7 +11,7 @@ unzip android-ndk-r10e-linux-x86_64.zip -n
 tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz --skip-old-files
 
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
-export PATH=/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH
+export PATH=$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH
 
 python3 ./waf configure -T release --prefix=../android_build --android=aarch64,host,21 --target=../android_build/aarch64 --disable-warns --togles
 python3 ./waf install --strip
