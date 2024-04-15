dfe4a66087441aa7fb6bbd89bc323f3eb77b5e61
HH45137 <3387640174@qq.com>
2024-04-15 22:07:58 +0800

    modified:   .github/workflows/build.yml 	modified:   scripts/build-android-arm64.sh 	modified:   scripts/build-android-armv7a.sh

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -8,9 +8,9 @@ unzip android-ndk-r10e-linux-x86_64.zip -n
 tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz --skip-old-files
 
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
-export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
+export PATH=/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH
 
-python3 ./waf configure -T release --prefix=/android_build --android=armeabi-v7a-hard,host,21 --target=/android_build/armeabi-v7a --disable-warns -4 --togles
+python3 ./waf configure -T release --prefix=../android_build --android=armeabi-v7a-hard,host,21 --target=../android_build/armeabi-v7a --disable-warns -4 --togles
 python3 ./waf install --strip
 
 # # 移除多余lib
