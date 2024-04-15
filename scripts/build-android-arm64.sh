73f878a4f537a3737f50a36797375c263aa9837f
HH45137 <3387640174@qq.com>
2024-04-15 21:14:30 +0800

    fix scripts

--- scripts/build-android-arm64.sh
+++ scripts/build-android-arm64.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 git submodule init && git submodule update
 
-wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -o /dev/null -nc    
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -o /dev/null -nc
+wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -nc    
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -nc
 
 unzip android-ndk-r10e-linux-x86_64.zip -n
 tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz --skip-old-files
@@ -10,5 +10,5 @@ tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz --skip-old-files
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
 
-./waf configure -T release --prefix=../android/build --android=aarch64,host,21 --target=../android/aarch64 --disable-warns --togles
-./waf install --strip
+python3 ./waf configure -T release --prefix=../android/build --android=aarch64,host,21 --target=../android/aarch64 --disable-warns --togles
+python3 ./waf install --strip
--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 git submodule init && git submodule update
 
-wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -o /dev/null -nc    
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -o /dev/null -nc
+wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -nc    
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -nc
 
 unzip android-ndk-r10e-linux-x86_64.zip -n
 tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz --skip-old-files
@@ -10,8 +10,8 @@ tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz --skip-old-files
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
 
-./waf configure -T release --prefix=../android/build --android=armeabi-v7a-hard,host,21 --target=../android/armeabi-v7a --disable-warns -4 --togles
-./waf install --strip
+python3 ./waf configure -T release --prefix=../android/build --android=armeabi-v7a-hard,host,21 --target=../android/armeabi-v7a --disable-warns -4 --togles
+python3 ./waf install --strip
 
 # # 移除多余lib
 # rm modlauncher-waf/android/lib/armeabi-v7a/libtier0.so
