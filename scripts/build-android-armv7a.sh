4d8ac169aa197b362e980d59fca4189357a5bd54
HH45137 <3387640174@qq.com>
2024-04-15 23:52:06 +0800

    no shit wget log

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -4,8 +4,8 @@ git submodule init && git submodule update
 sudo apt-get update
 sudo apt-get install -f -y libopenal-dev g++-multilib gcc-multilib libpng-dev libjpeg-dev libfreetype6-dev libfontconfig1-dev libcurl4-gnutls-dev libsdl2-dev zlib1g-dev libbz2-dev libedit-dev
 
-wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -nc    
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -nc
+wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -nc -q    
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -nc -q
 
 unzip -q android-ndk-r10e-linux-x86_64.zip
 tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
